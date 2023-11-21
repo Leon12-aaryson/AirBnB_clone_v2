b7f2e7c3e3a1f1d46efe30ce019d4b62c7d64eee
leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2023-11-21 22:51:18 +0300

    addition of cls=None to the a object

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,10 +8,12 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
-        """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
-
+    def all(self, cls=None):
+        """Returns a dictionary of objects"""
+        if cls is not None:
+            return {k: v for k, v in self.__objects.items() if isinstance(v, cls)}
+        return self.__objects
+    
     def new(self, obj):
         """Adds new object to storage dictionary"""
         self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
@@ -45,11 +47,10 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
-        
-        
+
     def delete(self, obj=None):
         """Deletes obj from __objects if it exists"""
         if obj is not None:
